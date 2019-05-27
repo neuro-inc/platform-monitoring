2ee9808c2d5dca7d84b7d0bd35faa86706777fce
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-05-27 18:14:54 +0300

    cleanup: rename

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import AsyncIterator
 
 import aiohttp
@@ -8,25 +9,23 @@ from platform_monitoring.config import Config
 from .conftest import ApiAddress, create_local_app_server
 
 
-class ApiConfigBase:
-    def __init__(self, address: ApiAddress) -> None:
-        self._address = address
+@dataclass(frozen=True)
+class ApiConfig:
+    address: ApiAddress
 
     @property
     def endpoint(self) -> str:
-        return f"http://{self._address.host}:{self._address.port}/api/v1"
+        return f"http://{self.address.host}:{self.address.port}/api/v1"
 
-
-class MonitoringApiConfig(ApiConfigBase):
     @property
     def ping_url(self) -> str:
         return f"{self.endpoint}/ping"
 
 
 @pytest.fixture
-async def api(config: Config) -> AsyncIterator[MonitoringApiConfig]:
+async def api(config: Config) -> AsyncIterator[ApiConfig]:
     async with create_local_app_server(config, port=8080) as api_config:
-        yield MonitoringApiConfig(api_config)
+        yield ApiConfig(api_config)
 
 
 @pytest.fixture
@@ -37,9 +36,7 @@ async def client() -> AsyncIterator[aiohttp.ClientSession]:
 
 class TestApi:
     @pytest.mark.asyncio
-    async def test_ping(
-        self, api: MonitoringApiConfig, client: aiohttp.ClientSession
-    ) -> None:
+    async def test_ping(self, api: ApiConfig, client: aiohttp.ClientSession) -> None:
         async with client.get(api.ping_url) as resp:
             assert resp.status == HTTPOk.status_code
             text = await resp.text()
