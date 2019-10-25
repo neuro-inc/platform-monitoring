a13457dbbb266ae988301557d27744f44cfcb9d1
Ilya Gruzinov <ilya.gruzinov@neuromation.io>
2019-10-25 08:58:08 +0300

    platform-logging integration (#119)

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ install_requires = (
     "aiohttp==3.6.2",
     'dataclasses==0.6; python_version<"3.7"',
     "yarl==1.3.0",
-    "neuro_auth_client==1.0.9",
+    "neuro_auth_client==1.0.10",
     "neuromation==19.7.4",
     "async-exit-stack==1.0.1",  # backport from 3.7 stdlib
     "aioelasticsearch==0.5.2",
     "aiodocker==0.17.0",
     "docker-image-py==0.1.10",
     "trafaret==1.2.0",
+    "platform-logging==0.3",
 )
 
 setup(
