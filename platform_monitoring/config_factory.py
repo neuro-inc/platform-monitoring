4ffa255294ca0951d3ba16e01dd1dcc53b0023aa
Serhiy Storchaka <storchaka@gmail.com>
2020-01-15 22:39:08 +0200

    Make NP_CLUSTER_NAME optional.

--- platform_monitoring/config_factory.py
+++ platform_monitoring/config_factory.py
@@ -26,8 +26,7 @@ class EnvironConfigFactory:
         self._environ = environ or os.environ
 
     def create(self) -> Config:
-        cluster_name = self._environ["NP_CLUSTER_NAME"]
-        # cluster_name = self._environ.get("NP_CLUSTER_NAME", "")
+        cluster_name = self._environ.get("NP_CLUSTER_NAME", "")
         return Config(
             server=self._create_server(),
             platform_api=self._create_platform_api(),
