33312d90c7c6c3d64e0a66f5e198837911cb969b
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-04-02 12:11:35 +0300

    Bump aiodocker to 0.18.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = (
     "neuro_auth_client==19.10.5",
     "neuromation==20.3.23",
     "aioelasticsearch==0.6.0",
-    "aiodocker==0.18.0",
+    "aiodocker==0.18.1",
     "docker-image-py==0.1.10",
     "trafaret==2.0.2",
     "platform-logging==0.3",
