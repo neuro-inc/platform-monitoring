2ffa29849ea478860ffeac9fdbc2bf42c3b598c2
Serhiy Storchaka <storchaka@gmail.com>
2021-04-23 12:25:50 +0300

    Bump neuro-auth-client from 19.9.5 to 21.4.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup_requires = ("setuptools_scm",)
 
 install_requires = (
     "aiohttp==3.7.4",
-    "neuro_auth_client==19.10.5",
+    "neuro_auth_client==21.4.19",
     "platform_config_client==21.4.20",
     "neuromation==20.12.7",
     "aioelasticsearch==0.7.0",
