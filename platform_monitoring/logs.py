118264c1ef0b1440f7b1259f772c5d3baae95a19
Ivan Zubenko <vanya.zubenko@gmail.com>
2021-05-11 17:31:57 +0300

    update doc type for elasticsearch logs

--- platform_monitoring/logs.py
+++ platform_monitoring/logs.py
@@ -193,7 +193,7 @@ class ElasticsearchLogReader(LogReader):
     ) -> None:
         self._es_client = es_client
         self._index = "logstash-*"
-        self._doc_type = "fluentd"
+        self._doc_type = "fluent-bit"
 
         self._namespace_name = namespace_name
         self._pod_name = pod_name
