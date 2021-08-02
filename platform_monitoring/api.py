4355cc94294b0c70542a8b75760e331f424765fc
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-08-02 16:59:43 +0300

    fix

--- platform_monitoring/api.py
+++ platform_monitoring/api.py
@@ -31,12 +31,6 @@ from aiohttp_security import check_authorized
 from aiohttp_security.api import AUTZ_KEY
 from neuro_auth_client import AuthClient, Permission
 from neuro_auth_client.security import AuthScheme, setup_security
-from neuro_sdk import (
-    Client as PlatformApiClient,
-    Factory as PlatformClientFactory,
-    JobDescription as Job,
-)
-from platform_config_client import ConfigClient
 from neuro_logging import (
     init_logging,
     make_sentry_trace_config,
@@ -46,6 +40,12 @@ from neuro_logging import (
     setup_zipkin,
     setup_zipkin_tracer,
 )
+from neuro_sdk import (
+    Client as PlatformApiClient,
+    Factory as PlatformClientFactory,
+    JobDescription as Job,
+)
+from platform_config_client import ConfigClient
 from yarl import URL
 
 from .base import JobStats, Telemetry
