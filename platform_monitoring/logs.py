86f4415e54ade88a385557aff74e06bc2bc4dc53
Serhiy Storchaka <storchaka@gmail.com>
2021-08-13 08:43:42 +0300

    Restore "# type: ignore".

--- platform_monitoring/logs.py
+++ platform_monitoring/logs.py
@@ -152,7 +152,7 @@ class PodContainerLogReader(LogReader):
     async def __aexit__(self, *args: Any) -> None:
         assert self._iterator
         if hasattr(self._iterator, "aclose"):
-            await self._iterator.aclose()
+            await self._iterator.aclose()  # type: ignore
         assert self._stream_cm
         stream_cm = self._stream_cm
         self._stream_cm = None
