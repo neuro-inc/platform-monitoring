0f6d48b3973764c45e9937dc9bbb95d62efd6321
Ivan Zubenko <ivan.zubenko@angloamerican.com>
2023-07-17 20:12:23 +0300

    add update kube token task

--- tests/integration/test_kube.py
+++ tests/integration/test_kube.py
@@ -2,29 +2,32 @@ from __future__ import annotations
 
 import asyncio
 import logging
+import os
 import re
+import tempfile
 import uuid
-from pathlib import Path
-from collections.abc import AsyncIterator, Callable, Coroutine
+from collections.abc import AsyncIterator, Callable, Coroutine, Iterator
 from contextlib import AbstractAsyncContextManager
 from datetime import datetime, timedelta, timezone
+from pathlib import Path
 from typing import Any, Union
 from unittest import mock
 from uuid import uuid4
 
+import aiohttp
+import aiohttp.web
 import pytest
 from aiobotocore.client import AioBaseClient
 from aioelasticsearch import Elasticsearch
 from aiohttp import web
 from async_timeout import timeout
-from yarl import URL
 
 from platform_monitoring.config import KubeConfig
 from platform_monitoring.kube_client import (
     JobNotFoundException,
     KubeClient,
-    KubeClientException,
     KubeClientAuthType,
+    KubeClientException,
     PodContainerStats,
     PodPhase,
 )
@@ -39,8 +42,8 @@ from platform_monitoring.logs import (
 )
 from platform_monitoring.utils import parse_date
 
+from .conftest import ApiAddress, create_local_app_server
 from .conftest_kube import MyKubeClient, MyPodDescriptor
-from tests.integration.conftest import ApiAddress, create_local_app_server
 
 logger = logging.getLogger(__name__)
 
@@ -72,15 +75,16 @@ async def mock_kubernetes_server() -> AsyncIterator[ApiAddress]:
     async def _gpu_metrics(request: web.Request) -> web.Response:
         return web.Response(content_type="text/plain")
 
-    def _unauthorized_gpu_metrics(
-    ) -> Callable[[web.Request], Coroutine[Any, Any, web.Response]]:
-
+    def _unauthorized_gpu_metrics() -> (
+        Callable[[web.Request], Coroutine[Any, Any, web.Response]]
+    ):
         async def _inner(request: web.Request) -> web.Response:
             auth_header = request.headers.get("Authorization", "")
             if auth_header.split(" ")[1] == "authorized":
                 return web.Response(content_type="text/plain")
             else:
                 return web.Response(status=401)
+
         return _inner
 
     def _create_app() -> web.Application:
@@ -129,6 +133,65 @@ def s3_log_service(
         bucket_name=s3_logs_bucket,
         key_prefix_format=s3_logs_key_prefix_format,
     )
+
+
+class TestKubeClientTokenUpdater:
+    @pytest.fixture
+    async def kube_app(self) -> aiohttp.web.Application:
+        async def _get_nodes(request: aiohttp.web.Request) -> aiohttp.web.Response:
+            auth = request.headers["Authorization"]
+            token = auth.split()[-1]
+            app["token"]["value"] = token
+            return aiohttp.web.json_response({"kind": "NodeList", "items": []})
+
+        app = aiohttp.web.Application()
+        app["token"] = {"value": ""}
+        app.router.add_routes([aiohttp.web.get("/api/v1/nodes", _get_nodes)])
+        return app
+
+    @pytest.fixture
+    async def kube_server(
+        self, kube_app: aiohttp.web.Application, unused_tcp_port_factory: Any
+    ) -> AsyncIterator[str]:
+        async with create_local_app_server(
+            kube_app, port=unused_tcp_port_factory()
+        ) as address:
+            yield f"http://{address.host}:{address.port}"
+
+    @pytest.fixture
+    def kube_token_path(self) -> Iterator[str]:
+        _, path = tempfile.mkstemp()
+        Path(path).write_text("token-1")
+        yield path
+        os.remove(path)
+
+    @pytest.fixture
+    async def kube_client(
+        self, kube_server: str, kube_token_path: str
+    ) -> AsyncIterator[KubeClient]:
+        async with KubeClient(
+            base_url=kube_server,
+            namespace="default",
+            auth_type=KubeClientAuthType.TOKEN,
+            token_path=kube_token_path,
+            token_update_interval_s=1,
+        ) as client:
+            yield client
+
+    async def test_token_periodically_updated(
+        self,
+        kube_app: aiohttp.web.Application,
+        kube_client: KubeClient,
+        kube_token_path: str,
+    ) -> None:
+        await kube_client.get_nodes()
+        assert kube_app["token"]["value"] == "token-1"
+
+        Path(kube_token_path).write_text("token-2")
+        await asyncio.sleep(2)
+
+        await kube_client.get_nodes()
+        assert kube_app["token"]["value"] == "token-2"
 
 
 class TestKubeClient:
@@ -473,24 +536,6 @@ class TestKubeClient:
             payload = await stream.read()
             assert payload == b""
 
-    async def test_get_node_proxy_client(
-        self, kube_config: KubeConfig, kube_client: MyKubeClient
-    ) -> None:
-        node_list = await kube_client.get_node_list()
-        node_name = node_list["items"][0]["metadata"]["name"]
-        async with kube_client.get_node_proxy_client(
-            node_name, KubeConfig.kubelet_node_port
-        ) as client:
-            assert client.url == URL(
-                kube_config.endpoint_url
-                + f"/api/v1/nodes/{node_name}:{KubeConfig.kubelet_node_port}/proxy"
-            )
-
-            async with client.session.get(URL(f"{client.url}/stats/summary")) as resp:
-                assert resp.status == 200, await resp.text()
-                payload = await resp.json()
-                assert "node" in payload
-
     async def test_get_nodes(self, kube_client: MyKubeClient) -> None:
         nodes = await kube_client.get_nodes()
         assert nodes
@@ -534,7 +579,7 @@ class TestKubeClient:
         [
             ["authorized", True],
             ["badtoken", False],
-        ]
+        ],
     )
     async def test_get_pod_container_gpu_stats_handles_unauthorized(
         self,
