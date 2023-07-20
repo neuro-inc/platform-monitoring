2ddc4e0ccdc259fd22a2306c0e1bfd2293a39d1a
Yevhenii Semendiak <semendyak@gmail.com>
2023-07-20 12:56:54 +0300

    add unittest to check log entries w.o. log key are handled

--- tests/unit/test_logs.py
+++ tests/unit/test_logs.py
@@ -1,5 +1,6 @@
+import json
 from collections.abc import AsyncIterator, Callable, Sequence
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import Any
 from unittest import mock
 
@@ -28,6 +29,27 @@ class TestS3LogReader:
             paginator.paginate.side_effect = paginate
             s3_client.get_paginator.return_value = paginator
 
+        return _setup
+
+    @pytest.fixture
+    def setup_s3_key_content(
+        self,
+        s3_client: mock.Mock,
+    ) -> Callable[[dict[str, list[str]]], None]:
+        def _setup(content: dict[str, list[str]]) -> None:
+            async def get_object(
+                Key: str, *args: Any, **kwargs: Any
+            ) -> dict[str, Any]:
+                async def _iter() -> AsyncIterator[str]:
+                    for line in content[Key]:
+                        yield line
+                body = mock.AsyncMock()
+                body.iter_lines = mock.MagicMock()
+                body.iter_lines.return_value = mock.AsyncMock()
+                body.iter_lines.return_value.__aiter__.side_effect = _iter
+                return {"ContentType": "", "Body": body}
+
+            s3_client.get_object = get_object
         return _setup
 
     @pytest.fixture
@@ -63,3 +85,31 @@ class TestS3LogReader:
             ]
             dt = datetime(2021, 1, 31, 12, 3, 0, tzinfo=timezone.utc)
             assert list(await log_reader._load_log_keys(dt)) == []
+
+    async def test_iterate_log_chunks(
+        self,
+        log_reader: S3LogReader,
+        setup_s3_pages: Callable[[Sequence[dict[str, Any]]], None],
+        setup_s3_key_content: Callable[[dict[str, Any]], None],
+    ) -> None:
+        now = datetime.now(tz=timezone.utc)
+        now_as_key = now.strftime("%Y%m%d%H%M")
+        setup_s3_pages([{"Contents": [{"Key": f"s3-key/{now_as_key}_0.gz"}]}])
+
+        log_lines = ["qwe\n", "line2", "line3", "", "\n\n\n", "something here"]
+
+        def later_iso(sec: int = 0) -> str:
+            return (now + timedelta(seconds=sec)).isoformat()
+        stored_lines = []
+        for i, line in enumerate(log_lines):
+            stored_line = {"time": later_iso(i)}
+            if line:
+                stored_line["log"] = line
+            stored_lines.append(json.dumps(stored_line))
+        setup_s3_key_content({f"s3-key/{now_as_key}_0.gz": stored_lines})
+
+        res = []
+        async with log_reader as it:
+            async for chunk in it:
+                res.append(chunk.decode()[:-1])  # -1 implies removal of \n
+        assert log_lines == res
