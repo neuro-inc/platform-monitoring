442f94bbaa9d8becec2985e023b6b493032e7f97
Ivan Zubenko <vanya.zubenko@gmail.com>
2024-08-09 10:56:40 +0200

    fix

--- platform_monitoring/api.py
+++ platform_monitoring/api.py
@@ -637,7 +637,7 @@ async def create_platform_api_client(
     platform_api_factory = PlatformClientFactory(
         tmp_config, trace_configs=trace_configs
     )
-    await platform_api_factory.login_with_token(url=url, token=token)
+    await platform_api_factory.login_with_token(url=url / "api/v1", token=token)
     client = None
     try:
         client = await platform_api_factory.get()
@@ -764,7 +764,7 @@ async def create_app(config: Config) -> aiohttp.web.Application:
             logger.info("Initializing Platform API client")
             platform_client = await exit_stack.enter_async_context(
                 create_platform_api_client(
-                    config.platform_api.url / "api/v1", config.platform_api.token
+                    config.platform_api.url, config.platform_api.token
                 )
             )
 
